102bb8090c71f4320ffc9d681a41d74400657e5c
Roopesh Saravanan <helloiamroopesh@gmail.com>
2021-03-01 14:31:45 +0530

    v1.5
    ninth commit | code refactored

--- script.js
+++ script.js
@@ -1,41 +1,62 @@
 'use strict'
 
-///////////////////////////////////////
-// Modal window
-
+// Modal window Selectors
 const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
+
+// button selectors
 const btnCloseModal = document.querySelector('.btn--close-modal')
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal')
 const btnScrollTo = document.querySelector('.btn--scroll-to')
+
+// section selectors
+const allSections = document.querySelectorAll('section')
 const section1 = document.querySelector('#section--1')
-const navLinks = document.querySelector('.nav__links')
+
+// tabs selectors
 const tabs = document.querySelectorAll('.operations__tab')
 const tabsContainer = document.querySelector('.operations__tab--container')
 const tabsContent = document.querySelectorAll('.operations__content')
+
+// nav selectors
 const nav = document.querySelector('.nav')
-const header = document.querySelector('.header')
+const navLinks = document.querySelector('.nav__links')
 const navHeight = nav.getBoundingClientRect().height
-const allSections = document.querySelectorAll('section')
+
+//header selector
+const header = document.querySelector('.header')
+
+// lazy loading image selector
+const imgTargets = document.querySelectorAll('img[data-src]')
+
+// slider selectors
 const slides = document.querySelectorAll('.slide')
 const sliderBtnRight = document.querySelector('.slider__btn--right')
 const sliderBtnLeft = document.querySelector('.slider__btn--left')
 const slider = document.querySelector('.slider')
-let currentSlide = 0
 const maxSlide = slides.length
+let currentSlide = 0
+
+// slider dot selectors
 const dotContainer = document.querySelector('.dots')
+
+// scroll to top button selectors
 const target = document.querySelector('.footer')
 const scrollToTopBtn = document.querySelector('.scroll-to-top-btn')
-const openModal = function (e) {
+const rootElement = document.documentElement
+
+function openModal(e) {
   e.preventDefault()
   modal.classList.remove('hidden')
   overlay.classList.remove('hidden')
 }
 
-const closeModal = function () {
+function closeModal() {
   modal.classList.add('hidden')
   overlay.classList.add('hidden')
 }
+
+// Event listeners
 
 btnsOpenModal.forEach((btn) => btn.addEventListener('click', openModal))
 
@@ -89,6 +110,7 @@ tabsContainer.addEventListener('click', (e) => {
 })
 
 // Menu fade
+
 function handleNavHover(e) {
   if (e.target.classList.contains('nav__link')) {
     const link = e.target
@@ -102,22 +124,12 @@ function handleNavHover(e) {
 }
 
 //Passing argument into handler function
+
 nav.addEventListener('mouseover', handleNavHover.bind(0.5))
 nav.addEventListener('mouseout', handleNavHover.bind(1))
 
-// Sticky navigation for nav bar
-
-// const initialCoords = section1.getBoundingClientRect()
-
-// window.addEventListener('scroll', (e) => {
-//   if (window.scrollY > initialCoords.top) {
-//     nav.classList.add('sticky')
-//   } else {
-//     nav.classList.remove('sticky')
-//   }
-// })
-
 // Sticky navigation for nav bar using Intersection Observer API
+
 function stickyNav(entries) {
   const [entry] = entries
   if (!entry.isIntersecting) {
@@ -126,21 +138,25 @@ function stickyNav(entries) {
     nav.classList.remove('sticky')
   }
 }
+
 const headerObserverOptions = {
   root: null,
   threshold: 0,
   rootMargin: `-${navHeight}px`,
 }
+
 const headerObserver = new IntersectionObserver(stickyNav, headerObserverOptions)
 headerObserver.observe(header)
 
 // Reveal sections
+
 function revealSection(entries, observer) {
   const [entry] = entries
   if (!entry.isIntersecting) return
   entry.target.classList.remove('section--hidden')
   observer.unobserve(entry.target)
 }
+
 const sectionObserverOptions = {
   root: null,
   threshold: 0.15,
@@ -153,7 +169,7 @@ allSections.forEach((section) => {
 })
 
 // Lazy loading images
-const imgTargets = document.querySelectorAll('img[data-src]')
+
 function loadImg(entries, observer) {
   const [entry] = entries
   if (!entry.isIntersecting) return
@@ -162,16 +178,19 @@ function loadImg(entries, observer) {
     entry.target.classList.remove('lazy-img')
   })
 }
+
 const loadImgOptions = {
   root: null,
   threshold: 0,
   rootMargin: '200px',
 }
+
 const imgObserver = new IntersectionObserver(loadImg, loadImgOptions)
 imgTargets.forEach((img) => imgObserver.observe(img))
 
 // Working on slides
 // Aligning all the slides horizontally using transform property in css
+
 sliderInit()
 
 slides.forEach((slide, index) => {
@@ -185,6 +204,7 @@ function goToSlide(slide) {
 }
 
 // Next Slide
+
 function nextSlide() {
   if (currentSlide === maxSlide - 1) {
     currentSlide = 0
@@ -196,6 +216,7 @@ function nextSlide() {
 }
 
 // Previous slide
+
 function previousSlide() {
   if (currentSlide == 0) {
     currentSlide = maxSlide - 1
@@ -221,6 +242,7 @@ document.addEventListener('keydown', (e) => {
 })
 
 // Creating dots for slides
+
 function createDots() {
   slides.forEach((_, i) => {
     const dotContainerHtml = `<button class="dots__dot" data-slide="${i}"></button>`
@@ -244,8 +266,6 @@ dotContainer.addEventListener('click', (e) => {
 })
 
 // Scroll to top implementation
-
-var rootElement = document.documentElement
 
 function scrollToTopCallback(entries, observer) {
   // The callback will return an array of entries, even if you are only observing a single item
